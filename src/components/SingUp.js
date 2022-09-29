5a6aba0a17efde6d60b8b65c57fb0cfcc1e51d41
Sanaz <szeraati69@gmail.com>
2022-09-29 23:56:59 +0330

    show errors and toastify

--- src/components/SingUp.js
+++ src/components/SingUp.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { validate } from './validate';
+import { notify } from './toast';
 
 const SingUp = () => {
   const [data, setData] = useState({
@@ -12,11 +15,12 @@ const SingUp = () => {
   });
 
   const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
 
   useEffect(() => {
     setErrors(validate(data));
-    console.log(errors);
-  }, [data]);
+    // console.log(errors);
+  }, [data, touched]);
 
   const changeHandler = (event) => {
     if (event.target.value === event.target.isAccepted) {
@@ -27,59 +31,99 @@ const SingUp = () => {
     console.log(data);
   };
 
+  const touchHandler = (event) => {
+    setTouched({ ...touched, [event.target.name]: true });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!Object.keys(errors).length) {
+      notify('You signed in successfully', 'success');
+    } else {
+      notify('Invalid data', 'error');
+      setTouched({
+        name: true,
+        email: true,
+        password: true,
+        confirmPassword: true,
+        isAccepted: true,
+      });
+    }
+  };
+
   return (
-    <form>
-      <h2>SingUp</h2>
-      <div>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={data.name}
-          onChange={changeHandler}
-        />
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type="text"
-          name="email"
-          value={data.email}
-          onChange={changeHandler}
-        />
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          value={data.password}
-          onChange={changeHandler}
-        />
-      </div>
-      <div>
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          value={data.confirmPassword}
-          onChange={changeHandler}
-        />
-      </div>
-      <div>
-        <label>I accept terms of policy</label>
-        <input
-          type="checkbox"
-          name="isAccepted"
-          value={data.isAccepted}
-          onChange={changeHandler}
-        />
-      </div>
-      <div>
-        <a href="#">Login</a>
-        <button type="submit">Sing Up</button>
-      </div>
-    </form>
+    <div>
+      <form onSubmit={submitHandler}>
+        <h2>SingUp</h2>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={changeHandler}
+            onFocus={touchHandler}
+          />
+          {errors.name && touched.name && <span>{errors.name}</span>}
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+            onFocus={touchHandler}
+          />
+          {errors.email && touched.email && <span>{errors.email}</span>}
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+            onFocus={touchHandler}
+          />
+          {errors.password && touched.password && (
+            <span>{errors.password}</span>
+          )}
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+            onFocus={touchHandler}
+          />
+          {errors.confirmPassword && touched.confirmPassword && (
+            <span>{errors.confirmPassword}</span>
+          )}
+        </div>
+        <div>
+          <label>I accept terms of policy</label>
+          <input
+            type="checkbox"
+            name="isAccepted"
+            value={data.isAccepted}
+            onChange={changeHandler}
+            onFocus={touchHandler}
+          />
+          {errors.isAccepted && touched.isAccepted && (
+            <span>{errors.isAccepted}</span>
+          )}
+        </div>
+        <div>
+          <a href="#">Login</a>
+          <button type="submit">Sing Up</button>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
   );
 };
 
